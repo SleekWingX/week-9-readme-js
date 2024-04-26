889bdab98c5264046803d9e536ec6f2e455e9238
Jack <alexisyazzie365@gmail.com>
2024-04-26 13:13:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+# title
+
+## Description
+blah blah blah
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+```
+install it, dummy
+```
+
+## Usage
+to do your best
+
+## Contributing
+fix my errors
+
+## License
+This project is licensed under the [object Object] license.
+
+## Tests
+testing testing 1 2 3
+
+## Questions
+If you have any questions please go to my github:
+https://github.com/SleekWingX 
+or email at:
+alexisyazzie365@gmail.com
---
+++ index.js
@@ -0,0 +1,133 @@
+// packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// an array of questions for user input
+const questions = [
+    {
+    type: 'input', // input takes user input for the data to be output later
+    name: 'title', // type of data used to specify what part of the array is being used
+    message: 'What is the title of your project?', // questions appear to user to prompt input
+    },
+    {
+    type: 'input',
+    name: 'description',
+    message: 'Please give a short description of your project.',
+    },
+    {
+    type: 'input',
+    name: 'installation',
+    message: 'How will the user install your program?',
+    },
+    {
+    type: 'input',
+    name: 'usage',
+    message: 'What is the functionality of the project?',
+    },
+    {
+    type: 'input',
+    name: 'contributing',
+    message: 'How can future developers contribute to your project?',
+    },
+    {
+        type: 'list',  // 'list' type used here to indicate dropdown list
+        name: 'license',
+        message: 'Please choose the license for your project:',
+        // lists licenses to choose from and their icon badge links (used shields.io for badges)
+        choices: [
+            { name: 'MIT', value: { name: 'MIT', badge: 'https://img.shields.io/badge/License-MIT-yellow.svg' }},
+            { name: 'Apache 2.0', value: { name: 'Apache 2.0', badge: 'https://img.shields.io/badge/License-Apache_2.0-blue.svg' }},
+            { name: 'GPL v3', value: { name: 'GPL v3', badge: 'https://img.shields.io/badge/License-GPLv3-blue.svg' }},
+            { name: 'BSD 3-Clause', value: { name: 'BSD 3-Clause', badge: 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg' }},
+            { name: 'None', value: { name: 'None', badge: '' }}
+        ]
+    },
+    {
+    type: 'input',
+    name: 'tests',
+    message: 'What tests can a user go through to troubleshoot the application?',
+    },
+    {
+    type: 'input',
+    name: 'github',
+    message: 'What is your github username?'
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: 'Please provide an email address to send questions to:'
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        // if an error occurs message appears with error attached
+        if (err) {
+            console.error('Error when writing file: ', err);
+        // if no error occurs then output that it was successful
+        } else {
+            console.log('Successfully wrote to', fileName);
+        }
+    });
+}
+
+// function to initialize app
+function init() {
+    // uses inquirer to take array to prompt user and take input
+    inquirer
+        .prompt(questions)
+        // creates the file content based on user input
+        .then ((answers) => {
+            // calls on the badge link from the answers variable
+            const badge = answers.license.badge ? `![License: ${answers.license.name}](${answers.license.badge})\n` : '';
+            const readMeContent = 
+            // uses badge variable and then title from the answers variable
+            `${badge}# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+\`\`\`
+${answers.installation}
+\`\`\`
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Tests
+${answers.tests}
+
+## Questions
+If you have any questions please go to my github:
+https://github.com/${answers.github} 
+or email at:
+${answers.email}
+`;
+        // calls the function to write the readme file
+            writeToFile('README.md', readMeContent.trim());
+        })
+        // sends out an error message if anything runs incorrectly
+        .catch(error => {
+            console.error('Error during inquirer prompt:', error);
+        });
+}
+
+// Function call to initialize app
+init();
